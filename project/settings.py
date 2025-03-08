8460d7420d995033669c191003d145104c9e179d
djinwoo <2021112390@dgu.ac.kr>
2025-03-08 19:21:39 +0900

    Feat:mainpage만들기

--- project/settings.py
+++ project/settings.py
@@ -114,7 +114,11 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'project', 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
